0be5d5f2286f411ce402764f0889f22989660e76
jayking <jayking@MacBook-Pro.local>
2024-04-22 11:16:47 +0900

    git init

---
+++ src/main/kotlin/org/example/thisandthat/ThisAndThatApplication.kt
@@ -0,0 +1,11 @@
+package org.example.thisandthat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThisAndThatApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThisAndThatApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/thisandthat/ThisAndThatApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.thisandthat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThisAndThatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
