435127766f338e0c0f4ea2f6d7d32f849e3700f5
jayking <mojunseo49@naver.com>
2024-04-22 15:11:05 +0900

    add: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.kotest:kotest-assertions-core")
+    testImplementation("io.kotest:kotest-runner-junit5")
+
 }
 
 tasks.withType<KotlinCompile> {
